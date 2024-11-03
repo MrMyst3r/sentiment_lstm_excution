eb26aeb5bd206f6a2a7690aad4e3fbec736a886a
Myster <149244323+MrMyst3r@users.noreply.github.com>
2024-11-03 22:17:35 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,3 +8,7 @@ pip install -r requirements.txt
 
 sudo apt-get update
 sudo apt-get install -y portaudio19-dev
+
+sudo apt-get install -y libportaudio2 libportaudiocpp0 portaudio19-dev
+
+sudo apt-get install -y build-essential
